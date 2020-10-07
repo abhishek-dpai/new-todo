d944c2dfd891805a3caa85c714d74e6acdded4c8
raiab <abhishekr@dailyprophet.ai>
2020-10-07 15:55:18 +0530

    corrected details popup message

--- src/components/DetailsForm.js
+++ src/components/DetailsForm.js
@@ -7,6 +7,18 @@ function DetailsForm(props) {
   //   console.log(detail.id);
   //   return detail.id === id;
   // });
+  let priorityMessage = "null";
+  switch (detail.priority) {
+    case 1:
+      priorityMessage = "High";
+      break;
+    case 2:
+      priorityMessage = "Medium";
+      break;
+    case 3:
+      priorityMessage = "Low";
+      break;
+  }
   console.log("props in detail form are", props);
   return (
     <main className="details-form-container">
@@ -15,7 +27,7 @@ function DetailsForm(props) {
           <h1>ID : {id}</h1>
           <h2>Description: {detail.description}</h2>
           <h1>Date: {detail.date}</h1>
-          <h1>Priority: {detail.priority}</h1>
+          <h1>Priority: {priorityMessage}</h1>
           <button>Close</button>
         </form>
       )) || <div>No details available</div>}
